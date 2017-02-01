c0a5c311a962b7bde5f054ce0e580ec9657cc011
selim <selim@wearkinetic.com>
2017-02-01 13:54:37 -0500

    added functions

--- cache.go
+++ cache.go
@@ -45,6 +45,35 @@ func (t *Trv) ShallowSave(keys ...string) *Trv {
 	return t
 }
 
+func (t *Trv) ShallowSaveF(f func(key string) (bool, string)) *Trv {
+
+	// Deep Calls
+	if t.isDeep {
+		for _, nestedTrv := range t.trvs {
+			nestedTrv.ShallowSaveF(f)
+		}
+		return t
+	}
+
+	// Loop over every node in the result
+	for nodeKey, node := range t.result {
+		_, exists := t.cache[nodeKey]
+		if !exists {
+			t.cache[nodeKey] = map[string]interface{}{}
+		}
+
+		// Loop over every key we care about
+		for key, value := range node.GetProps() {
+			keep, newKey := f(key)
+			if keep {
+				t.cache[nodeKey][newKey] = value
+			}
+		}
+	}
+	return t
+
+}
+
 // DeepSave the outermost cache the the level just below it
 func (t *Trv) DeepSave(name string) *Trv {
 
--- filters.go
+++ filters.go
@@ -1,7 +1,30 @@
 package askgo
 
+import (
+	"github.com/hippoai/goutil"
+	"github.com/hippoai/graphgo"
+)
+
 // HasResult is a built-in filter
 // Returns true if and only if the current traversal is non-empty
 func HasResult(trv *Trv, path []*Step) bool {
 	return trv.Size() > 0
+}
+
+// IsInValuesString returns a shallow filter
+// that will return true iff value is in given values
+func IsInValuesString(key string, values ...string) func(graphgo.INode, []*Step) bool {
+	return func(iNode graphgo.INode, path []*Step) bool {
+		vItf, err := iNode.Get(key)
+		if err != nil {
+			return false
+		}
+
+		v, ok := vItf.(string)
+		if !ok {
+			return false
+		}
+
+		return goutil.IsIn(v, values...)
+	}
 }
