58714eab9822c1c2497b5ce0cfb2282f8e05a93c
selim <selim@wearkinetic.com>
2017-03-08 16:11:52 -0500

    option to return slices

--- access.go
+++ access.go
@@ -2,14 +2,26 @@ package askgo
 
 import "github.com/hippoai/graphgo"
 
+// Size returns the size of the result
 func (t *Trv) Size() int {
 	return len(t.result)
 }
 
+// Result returns the nodes we found
 func (t *Trv) Result() map[string]graphgo.INode {
 	return t.result
 }
 
+// Return returns the cache as a map
 func (t *Trv) Return() map[string](map[string]interface{}) {
 	return t.cache
+}
+
+// ReturnSlice returns the cache as a slice and not a map
+func (t *Trv) ReturnSlice() []map[string]interface{} {
+	r := []map[string]interface{}{}
+	for _, value := range t.cache {
+		r = append(r, value)
+	}
+	return r
 }
